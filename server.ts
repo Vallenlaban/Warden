c35a2b7bc2afc1c7e55cf45af2fea6e6881b57ae
Vallenlaban <vallenlabansilaban@gmail.com>
2026-08-04 16:59:23 +0700

    Fix Vercel serverless routing config

--- server.ts
+++ server.ts
@@ -1,14 +1,11 @@
 ﻿import "dotenv/config";
-import express, { NextFunction, Request, Response } from "express";
-import path from "node:path";
-import fs from "node:fs";
-import { fileURLToPath } from "node:url";
+import express = require("express");
+import { NextFunction, Request, Response } from "express";
+import * as path from "path";
+import * as fs from "fs";
 import { GoogleGenAI } from "@google/genai";
-import nodeFetch from "node-fetch";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const fetch = globalThis.fetch ?? nodeFetch;
+const fetch = globalThis.fetch ?? require("node-fetch");
 const app = express();
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 const ROOT_DIR = process.cwd();
